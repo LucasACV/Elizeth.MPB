9cfcb5e97ae31f9b3afb2bf64705ddbe6d5b359d
LucasACV <lucas.valdez@sptech.school>
2025-05-16 17:47:55 -0300

    alteração de inputs

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,5 +1,4 @@
-var usuarioModel = require("../models/usuarioModel");
-var aquarioModel = require("../models/aquarioModel");
+var usuarioModel = require("../models/usuarioModel")
 
 function autenticar(req, res) {
     var email = req.body.emailServer;
@@ -56,7 +55,15 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
-    var fkEmpresa = req.body.idEmpresaVincularServer;
+    var sexo = req.body.sexoServer;
+    var idade = req.body.idadeServer
+
+    console.log(nome);
+    console.log(email);
+    console.log(senha);
+    console.log(sexo);
+    console.log(idade);
+    
 
     // Faça as validações dos valores
     if (nome == undefined) {
@@ -65,12 +72,10 @@ function cadastrar(req, res) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else if (fkEmpresa == undefined) {
-        res.status(400).send("Sua empresa a vincular está undefined!");
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, fkEmpresa)
+        usuarioModel.cadastrar(nome,idade,sexo,email,senha)
             .then(
                 function (resultado) {
                     res.json(resultado);
